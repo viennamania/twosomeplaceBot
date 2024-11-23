7d156464c5f3a543060f59856c2e788c542c1008
seunghyonpark <creath.park@gmail.com>
2024-11-23 13:01:27 +0900

    Enhance bot functionality with custom context and multiple commands

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -2,7 +2,12 @@ export const dynamic = 'force-dynamic'
 
 export const fetchCache = 'force-no-store'
 
-import { Bot, webhookCallback } from 'grammy'
+import {
+    Bot,
+    webhookCallback,
+    Context,
+    InlineKeyboard,
+} from 'grammy'
 
 import Replicate from "replicate";
 import * as fal from "@fal-ai/serverless-client";
@@ -16,6 +21,8 @@ import {
 
 
 
+//import { createToken } from "./jwt.js";
+//import { registerUser } from "./operations.js";
 
 
 
@@ -36,12 +43,246 @@ const token = process.env.TELEGRAM_BOT_TOKEN
 
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
-const bot = new Bot(token)
+
+
+//const bot = new Bot(token)
+
+
+
+
+
+const BOT_DEVELOPER = 441516803; // bot developer chat identifier // waynepark
+
+// Define custom context type.
+interface BotConfig {
+  botDeveloper: number;
+  isDeveloper: boolean;
+}
+type MyContext = Context & {
+  config: BotConfig;
+};
+
+const bot = new Bot<MyContext>(token);
+
+// Set custom properties on context objects.
+bot.use(async (ctx, next) => {
+  ctx.config = {
+    botDeveloper: BOT_DEVELOPER,
+    isDeveloper: ctx.from?.id === BOT_DEVELOPER,
+  };
+  await next();
+});
+
+
+
+
+
+
+
+// Commands - Start, Play, Leaderboard, About, Help
+bot.command("start", async (ctx) => {
+    
+    //const userDocId = await registerUser(ctx.from?.id.toString() || "", ctx.from?.first_name || "", ctx.from?.last_name || "", ctx.from?.username || "");
+    //console.log("User doc id:", userDocId);
+    
+    ctx.replyWithPhoto("https://img.etimg.com/thumb/msid-106967420,width-300,height-225,imgsize-478624,resizemode-75/my-life-with-the-walter-boys-season-2-see-everything-we-know-about-renewal-production-plot-and-more.jpg", {
+        
+        //"caption": "<b>Hi, " + ctx.from?.first_name + "</b>"
+        //+ "<p>Play the game now and become top players in the leaderboard!!!</p>",
+
+        "parse_mode": "HTML"
+    })
+});
+
+/*
+{
+  method: 'sendGame',
+  payload: [Object],
+  ok: false,
+  error_code: 400,
+  description: 'Bad Request: wrong game short name specified',
+  parameters: {}
+}
+*/
+
+
+bot.command("play", async (ctx) => {
+
+    //const userDocId = await registerUser(ctx.from?.id.toString() || "", ctx.from?.first_name || "", ctx.from?.last_name || "", ctx.from?.username || "");
+    //console.log("User doc id:", userDocId);
+    
+    const keyboard = new InlineKeyboard().game("Play now!")
+        .row()
+        .text("Leaderboard", "leaderboard")
+        .text("About", "about");
+
+    
+    ctx.replyWithGame("twosome", {
+        reply_markup: keyboard,
+        protect_content: true,
+        disable_notification: true
+    });
+
+
+
+});
+
+bot.command("leaderboard", (ctx) => {
+    ctx.reply("Leaderboard!!! \n1. User1\n2. User2\n3. User3");
+});
+bot.command("about", (ctx) => {
+    ctx.reply("About!!! \nThis is a game bot");
+});
+bot.command("help", (ctx) => {
+    ctx.reply(`
+    <b>Settle Mints Game Bot Help</b><br>
+    <p>Get in touch with the game support team.</p>
+  `, { parse_mode: "HTML" })
+});
+bot.on("callback_query:data", async (ctx) => {
+    const data = ctx.callbackQuery.data;
+    if (data === "leaderboard") {
+        ctx.reply("Leaderboard!!! \n1. User1\n2. User2\n3. User3");
+    } else if (data === "about") {
+        ctx.reply("About!!! \nThis is a game bot");
+    }
+});
+
+bot.on("callback_query:game_short_name", async (ctx) => {
+    
+    //const token = createToken(ctx.from.id.toString());
+
+    //console.log("Token: " + token);
+
+    const token = "1234567890";
+
+    await ctx.answerCallbackQuery({ url: `https://preview.codecanyon.net/item/coin-flip-land-html5/full_screen_preview/55506571?_ga=2.230819288.636301431.1732326489-1387678474.1732326489/?token=${token}` });
+
+});
+
+bot.catch((err) => console.error(err));
+
+
+
+
+
+
+
+
+
+
+
+bot.command("start", async (ctx) => {
+    // You can get the chat identifier of the user to send your game to with `ctx.from.id`.
+    // which gives you the chat identifier of the user who invoked the start command.
+    
+    //const chatId = ctx.from.id;
+
+    //await ctx.api.sendGame(chatid, "my_game");
+
+    const chatId = ctx.message?.chat?.id;
+
+    if (!chatId) return;
+
+    // Send a game to the chat
+    //await ctx.api.sendGame(chatId, "my_game");
+    await ctx.reply("Hi! I can only read messages that explicitly reply to me!", {
+        // Make Telegram clients automatically show a reply interface to the user.
+        reply_markup: { force_reply: true },
+    });
+
+});
+
+
+
+
+// Define handlers for custom context objects.
+/*
+bot.command("start", async (ctx) => {
+  if (ctx.config.isDeveloper) await ctx.reply("Hi mom!");
+  else await ctx.reply("Welcome");
+});
+*/
+
+
+
+
+bot.hears("ping", async (ctx) => {
+    // `reply` is an alias for `sendMessage` in the same chat (see next section).
+    await ctx.reply("pong", {
+        // `reply_parameters` specifies the actual reply feature.
+        reply_parameters:{
+                message_id: ctx.msg.message_id
+        },
+    });
+});
+
 
 bot.on('message:text', async (ctx) => {
 
 
-    console.log(ctx.message.text)
+    //console.log(ctx.message)
+    /*
+    {
+        message_id: 149,
+        from: {
+            id: 441516803,
+            is_bot: false,
+            first_name: 'Wayne',
+            last_name: 'Park',
+            username: 'waynepark',
+            language_code: 'ko'
+        },
+        chat: {
+            id: 441516803,
+            first_name: 'Wayne',
+            last_name: 'Park',
+            username: 'waynepark',
+            type: 'private'
+        },
+        date: 1732318838,
+        text: '안녕'
+    }
+    */
+
+    //await ctx.reply('Hello!')
+
+    await ctx.reply("Hello!", {
+        // `reply_parameters` specifies the actual reply feature.
+        reply_parameters:{
+                message_id: ctx.msg.message_id
+        },
+    });
+
+    ///await bot.api.sendMessage(441516803, "Hi!", {/* more options */});
+    /*
+    await bot.api.sendMessage(
+        ctx.message.chat.id,
+        
+        "*Hi\\!* _Welcome_ to [grammY](https://grammy.dev)\\.",
+
+        { parse_mode: "MarkdownV2" },
+    );
+    */
+
+
+    await bot.api.sendMessage(
+        ctx.message.chat.id,
+
+        '<b>Hi!</b> <i>Welcome</i> to <a href="https://gold.goodtether.com">GoodTether</a>.',
+
+        { parse_mode: "HTML" },
+    );
+
+    await bot.api.sendMessage(
+        ctx.message.chat.id,
+        'https://gold.goodtether.com',
+        {
+            parse_mode: "HTML",
+        },
+    );
+
+
 
     if (ctx.message.text === '/start') {
 
@@ -417,10 +658,225 @@ bot.on('message:photo', async (ctx) => {
 
  
     
-    ctx.api.close()
+    ////ctx.api.close()
 
 
 
 } )
+
+
+
+
+// message is video
+bot.on('message:video', async (ctx) => {
+
+    console.log("message:video", ctx.message.video)
+    /*
+    {
+        file_id: 'BAACAgUAAxkBAAM9Z0Bq4By_0DSwiBr18_SXceWmVIsAAmTDMRsJTwABVgaKqMNcfad3AQADAgADeQADNgQ',
+        file_unique_id: 'AQADZMMxGwlPAAFWfg',
+        file_size: 156830,
+        file_path: 'videos/file_0.mp4'
+        }
+    */
+   // get the video url
+    const videoUrl = `https://api.telegram.org/file/bot${token}/${ctx.message.video.file_path}`
+
+    console.log("videoUrl", videoUrl)
+
+    await ctx.reply('This is a video!')
+
+    ////ctx.api.close()
+
+} )
+
+
+bot.command("start", async (ctx) => {
+    await ctx.reply("Hi! I can only read messages that explicitly reply to me!", {
+      // Make Telegram clients automatically show a reply interface to the user.
+      reply_markup: { force_reply: true },
+    });
+});
+
+
+bot.on("message", async (ctx) => {
+  // `ctx` is the `Context` object.
+  console.log(ctx.message);
+});
+
+bot.on("edited_message", async (ctx) => {
+    // Get the new, edited, text of the message.
+    const editedText = ctx.editedMessage.text;
+
+    console.log("editedText", editedText)
+});
+
+
+/*
+bot.on("message", async (ctx) => {
+  await ctx.reply("I got your message!");
+});
+
+// Or, even shorter:
+bot.on("message", (ctx) => ctx.reply("Gotcha!"));
+*/
+
+
+
+/*
+{
+  message_id: 257,
+  from: {
+    id: 441516803,
+    is_bot: false,
+    first_name: 'Wayne',
+    last_name: 'Park',
+    username: 'waynepark',
+    language_code: 'ko'
+  },
+  chat: {
+    id: 441516803,
+    first_name: 'Wayne',
+    last_name: 'Park',
+    username: 'waynepark',
+    type: 'private'
+  },
+  date: 1732324028,
+  game: {
+    title: 'Math Battle',
+    text: 'Top Players\n1. Wayne Park – 5\n\nWayne has just scored 5.',
+    text_entities: [ [Object], [Object], [Object] ],
+    description: 'Show your math skills! How high can you score?',
+    photo: [ [Object], [Object], [Object] ]
+  },
+  reply_markup: { inline_keyboard: [ [Array] ] },
+  via_bot: {
+    id: 166035794,
+    is_bot: true,
+    first_name: 'GameBot',
+    username: 'gamebot'
+  }
+}
+*/
+
+
+/*
+{
+  message_id: 311,
+  from: {
+    id: 441516803,
+    is_bot: false,
+    first_name: 'Wayne',
+    last_name: 'Park',
+    username: 'waynepark',
+    language_code: 'ko'
+  },
+  chat: {
+    id: 441516803,
+    first_name: 'Wayne',
+    last_name: 'Park',
+    username: 'waynepark',
+    type: 'private'
+  },
+  date: 1732324684,
+  animation: {
+    file_name: '21.mp4',
+    mime_type: 'video/mp4',
+    duration: 1,
+    width: 848,
+    height: 848,
+    thumbnail: {
+      file_id: 'AAMCAgADGQEAAgE2Z0Es6ymKOVUw5FZ7xuX-yThkcLYAAo0PAAKO6UhIRRMoHr3lFbsBAAdtAAM2BA',
+      file_unique_id: 'AQADjQ8AAo7pSEhy',
+      file_size: 15350,
+      width: 320,
+      height: 320
+    },
+    thumb: {
+      file_id: 'AAMCAgADGQEAAgE2Z0Es6ymKOVUw5FZ7xuX-yThkcLYAAo0PAAKO6UhIRRMoHr3lFbsBAAdtAAM2BA',
+      file_unique_id: 'AQADjQ8AAo7pSEhy',
+      file_size: 15350,
+      width: 320,
+      height: 320
+    },
+    file_id: 'CgACAgIAAxkBAAIBNmdBLOspijlVMORWe8bl_sk4ZHC2AAKNDwACjulISEUTKB695RW7NgQ',
+    file_unique_id: 'AgADjQ8AAo7pSEg',
+    file_size: 100287
+  },
+  document: {
+    file_name: '21.mp4',
+    mime_type: 'video/mp4',
+    thumbnail: {
+      file_id: 'AAMCAgADGQEAAgE2Z0Es6ymKOVUw5FZ7xuX-yThkcLYAAo0PAAKO6UhIRRMoHr3lFbsBAAdtAAM2BA',
+      file_unique_id: 'AQADjQ8AAo7pSEhy',
+      file_size: 15350,
+      width: 320,
+      height: 320
+    },
+    thumb: {
+      file_id: 'AAMCAgADGQEAAgE2Z0Es6ymKOVUw5FZ7xuX-yThkcLYAAo0PAAKO6UhIRRMoHr3lFbsBAAdtAAM2BA',
+      file_unique_id: 'AQADjQ8AAo7pSEhy',
+      file_size: 15350,
+      width: 320,
+      height: 320
+    },
+    file_id: 'CgACAgIAAxkBAAIBNmdBLOspijlVMORWe8bl_sk4ZHC2AAKNDwACjulISEUTKB695RW7NgQ',
+    file_unique_id: 'AgADjQ8AAo7pSEg',
+    file_size: 100287
+  },
+  via_bot: {
+    id: 140267078,
+    is_bot: true,
+    first_name: 'Tenor GIF Search',
+    username: 'gif'
+  }
+}
+  */
+
+
+/*
+{
+  message_id: 312,
+  from: {
+    id: 441516803,
+    is_bot: false,
+    first_name: 'Wayne',
+    last_name: 'Park',
+    username: 'waynepark',
+    language_code: 'ko'
+  },
+  chat: {
+    id: 441516803,
+    first_name: 'Wayne',
+    last_name: 'Park',
+    username: 'waynepark',
+    type: 'private'
+  },
+  date: 1732324739,
+  video_note: {
+    duration: 3,
+    length: 384,
+    thumbnail: {
+      file_id: 'AAMCBQADGQEAAgE4Z0Etgy3_1RH7LmoEP_TDVOsK-5sAAm4kAAJaBhFWaN9PjlZBPJoBAAdtAAM2BA',
+      file_unique_id: 'AQADbiQAAloGEVZy',
+      file_size: 17269,
+      width: 320,
+      height: 320
+    },
+    thumb: {
+      file_id: 'AAMCBQADGQEAAgE4Z0Etgy3_1RH7LmoEP_TDVOsK-5sAAm4kAAJaBhFWaN9PjlZBPJoBAAdtAAM2BA',
+      file_unique_id: 'AQADbiQAAloGEVZy',
+      file_size: 17269,
+      width: 320,
+      height: 320
+    },
+    file_id: 'DQACAgUAAxkBAAIBOGdBLYMt_9UR-y5qBD_0w1TrCvubAAJuJAACWgYRVmjfT45WQTyaNgQ',
+    file_unique_id: 'AgADbiQAAloGEVY',
+    file_size: 250157
+  }
+}
+  */
+
+
 
 export const POST = webhookCallback(bot, 'std/http')
